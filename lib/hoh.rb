13f415e2552b513dfd6a7b795b8a7d162367f34d
Scott Hoisington <hoyzington@gmail.com>
2019-11-05 11:40:25 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -28,24 +28,8 @@ PORTION_1 = {
 
 def naming_system
   linear = PORTION_1
-  #linear[:sub_category][:sub_category][:sub_category] = PORTION_2
-  #linear[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
-  #linear[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
-  
-  linear[1][1][1] = PORTION_2
-  linear[1][1][1][1] = PORTION_3
-  linear[1][1][1][1][1][1] = PORTION_4
-  
+  linear[:sub_category][:sub_category][:sub_category] = PORTION_2
+  linear[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
+  linear[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
   linear
-  
-  
-  
-  # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
 end
